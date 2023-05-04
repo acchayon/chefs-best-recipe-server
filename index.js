0e6dc3f72d329de2ecc61d8c6bc5b6c37c61131a
acchayon <adhikaryac81@gmail.com>
2023-05-04 21:08:32 +0600

    vercel config added

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ app.get('/recipe', (req, res) => {
     res.send(recipe)
 })
 
+app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id)
+    if(id === 0){
+        res.send(chefs)
+    }
+    else{
+        const selectedChef = chefs.filter(c => parseInt(c.id) === id)
+        res.send(selectedChef) 
+    }
+})
+
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedRecipe = recipe.find(r => r.id == id);
+    res.send(selectedRecipe)  
+})
 
 app.listen(port, () => {
     console.log(`Chef server api is running on port: ${port}`)
